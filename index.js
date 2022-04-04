2ec9021673f997e5ab58edf6989182cc138463ca
simelane-jpd <201575227@student.uj.ac.za>
2022-04-04 13:12:11 +0200

    added content in index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add code in here to create an API with ExpressJS
+const express = require('express');
+const app = express();
+
+// enable the static folder...
+app.use(express.static('public'));
+
+// import the dataset to be used here
+
+const PORT = process.env.PORT || 4017;
+
+// API routes to be added here
+
+app.listen(PORT, function() {
+	console.log(`App started on port ${PORT}`)
+});
