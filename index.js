17f20df506365e621cbb5e50466b0a152fa1ad7f
simelane-jpd <201575227@student.uj.ac.za>
2022-04-20 15:07:31 +0200

    fixed adding garment button

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-// add code in here to create an API with ExpressJS
-
 const express = require('express');
+//const { get } = require('express/lib/request');
 const app = express();
+const fs = require("fs");
 
 // enable the static folder...
 app.use(express.static('public'));
+// enable the req.body object
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 // import the dataset to be used here
 const garments = require('./garments.json');
@@ -12,54 +16,29 @@ const garments = require('./garments.json');
 const PORT = process.env.PORT || 4017;
 
 // API routes to be added here
-//app.get('/api/garments', function(req, res){
-    // note that this route just send JSON data to the browser
-    // there is no template
-   // res.json({garments});
-//});
-
-app.listen(PORT, function() {
-	console.log(`App started on port ${PORT}`)
-});
-//add filter to route
-app.get('/api/garments', function(req, res){
-
+app.get('/api/garments', function (req, res) {
 	const gender = req.query.gender;
 	const season = req.query.season;
 
 	const filteredGarments = garments.filter(garment => {
 		// if both gender & season was supplied
 		if (gender != 'All' && season != 'All') {
-			return garment.gender === gender 
-				&& garment.season === season;
-		} else if(gender != 'All') { // if gender was supplied
 			return garment.gender === gender
-		} else if(season != 'All') { // if season was supplied
+				&& garment.season === season;
+		} else if (gender != 'All') { // if gender was supplied
+			return garment.gender === gender
+		} else if (season != 'All') { // if season was supplied
 			return garment.season === season
 		}
 		return true;
 	});
-
 	// note that this route just send JSON data to the browser
 	// there is no template
-	res.json({ 
-		garments : filteredGarments
-	});
+	res.json({ garments: filteredGarments });
 });
-
-function filterData() {
-	axios
-		.get(`/api/garments?gender=${genderFilter}&season=${seasonFilter}`)
-		.then(function(result) {
-			searchResultsElem.innerHTML = garmentsTemplate({
-				garments : result.data.garments
-			})
-		});
-}
-//price filter to route
-app.get('/api/garments/price/:price', function(req, res){
+app.get('/api/garments/price/:price', function (req, res) {
 	const maxPrice = Number(req.params.price);
-	const filteredGarments = garments.filter( garment => {
+	const filteredGarments = garments.filter(garment => {
 		// filter only if the maxPrice is bigger than maxPrice
 		if (maxPrice > 0) {
 			return garment.price <= maxPrice;
@@ -67,19 +46,10 @@ app.get('/api/garments/price/:price', function(req, res){
 		return true;
 	});
 
-res.json({ 
-		garments : filteredGarments
+	res.json({
+		garments: filteredGarments
 	});
 });
-
-// enable the req.body object - to allow us to use HTML forms
-// when doing post requests
-// put this before you declare any routes
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-//post route thunder client
 app.post('/api/garments', (req, res) => {
 
 	// get the fields send in from req.body
@@ -95,7 +65,7 @@ app.post('/api/garments', (req, res) => {
 	// only 3 fields are made mandatory here
 	// you can change that
 
-	if (!description || !img || !price) {
+	if (!description || !img || !price || !gender || !season) {
 		res.json({
 			status: 'error',
 			message: 'Required data not supplied',
@@ -103,28 +73,33 @@ app.post('/api/garments', (req, res) => {
 	} else {
 
 		// you can check for duplicates here using garments.find
-		// you can check for duplicates here using garments.find
 		if (garments.find(element => element.description === description)) {
 			res.json({
 				status: 'error',
 				message: 'Item already exists',
 			});
 		}
-		// add a new entry into the garments list
 		else {
-		garments.push({
-			description,
-			img,
-			gender,
-			season,
-			price
-		});
-		fs.writeFileSync('./garments.json', JSON.stringify(garments))
-		res.json({
-			status: 'success',
-			message: 'New garment added.',
-		});
-	}
-}
 
+			// add a new entry into the garments list
+			garments.push({
+				description,
+				img,
+				gender,
+				season,
+				price
+			});
+			//adding garments into the JSON file
+			fs.writeFileSync('./garments.json', JSON.stringify(garments))
+
+			res.json({
+				status: 'success',
+				message: 'New garment added.',
+			});
+		}
+	}
+
+});
+app.listen(PORT, function () {
+	console.log(`App started on port ${PORT}`)
 });
--- public/add-garments.js
+++ public/add-garments.js
@@ -1,74 +1,71 @@
-
-
-const popmsg = document.querySelector('.popmsg');
-const addGarmentButton = document.querySelector('.addGarmentButton');
-const hideAddGarmentButton = document.querySelector('.hideAddGarmentButton');
-const addClothes = document.querySelector('.add.clothes');
-const addClothesButton = document.querySelector('.add.button');
+const message = document.querySelector('.message');
+const addGarmetBtn = document.querySelector('.addGarmentBtn');
+const hideAddGarmetBtn = document.querySelector('.hideAddGarmetBtn');
+const addGarmetSection = document.querySelector('.add.garment');
+const addGarmetButtonSection = document.querySelector('.add.button');
 
 function showMessage(value) {
-    popmsg.innerHTML = value;
-     message.classList.toggle('hidden');
+	message.innerHTML = value;
+	 message.classList.toggle('hidden');
 
-    setTimeout(() => {
-      message.innerHTML = '';
-      message.classList.toggle('hidden');
-     }, 3000);
+	setTimeout(() => {
+	 	message.innerHTML = '';
+	 	message.classList.toggle('hidden');
+	 }, 3000);
 }
 
-function clothesScreen() {
-    addClothes.classList.toggle('hidden');
-     addGarmetButtonSection.classList.toggle('hidden');
+function toggleAddGarmetScreen() {
+	addGarmetSection.classList.toggle('hidden');
+	// addGarmetButtonSection.classList.toggle('hidden');
 }
 
-hideAddGarmentButton.addEventListener('click', function (evt) {
-    toggleclothesScreen()
+hideAddGarmetBtn.addEventListener('click', function (evt) {
+	toggleAddGarmetScreen()
 });
 
 const fieldManager = FieldManager({
-    'description': '',
-    'img': '',
-    'season': '',
-    'gender': '',
-    'price': 0.00
+	'description': '',
+	'img': '',
+	'season': '',
+	'gender': '',
+	'price': 0.00
 });
 
-addGarmentButton.addEventListener('click', function (evt) {
-    
-    // fields on the screen
-    const fields = fieldManager.getValues();
+addGarmetBtn.addEventListener('click', function (evt) {
+	
+	// fields on the screen
+	const fields = fieldManager.getValues();
 
-    axios
-        .post('/api/garments', fields)
-        .then(result => {
-            if (result.data.status == 'error') {
-                showMessage(result.data.popmsg);
-            } else {
-                clothesScreen();
-                // show success message from API
-                showMessage(result.data.message);
-                fieldManager.clear();
-// show all the data
-filterData();
-}
-})
-.catch(err => {
-showMessage(err.stack)
-});
+	axios
+		.post('/api/garments', fields)
+		.then(result => {
+			if (result.data.status == 'error') {
+				showMessage(result.data.message);
+			} else {
+				toggleAddGarmetScreen();
+				// show success message from API
+				showMessage(result.data.message);
+				fieldManager.clear();
+				// show all the data
+				filterData();
+			}
+		})
+		.catch(err => {
+			showMessage(err.stack)
+		});
 });
 
-addClothes.addEventListener('click', function (evt) {
-evt.preventDefault();
-toggleclothesScreen()
+addGarmetButtonSection.addEventListener('click', function (evt) {
+	evt.preventDefault();
+	toggleAddGarmetScreen()
 });
 function myFunction() {
-// Get the snackbar DIV
-var x = document.getElementById("snackbar");
-
-// Add the "show" class to DIV
-x.className = "show";
-
-// After 3 seconds, remove the show class from DIV
-setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}
-
+	// Get the snackbar DIV
+	var x = document.getElementById("snackbar");
+  
+	// Add the "show" class to DIV
+	x.className = "show";
+  
+	// After 3 seconds, remove the show class from DIV
+	setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
